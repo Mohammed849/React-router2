409bedc92c13fbba21fecb5d5235f801f76d2673
Mohammed849 <65894073+Mohammed849@users.noreply.github.com>
2020-08-12 16:07:29 +0200

    React-router2

--- src/Manga.js
+++ src/Manga.js
@@ -1,12 +1,27 @@
 import React from 'react';
 class Manga extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      author: '',
+    };
+  }
   componentDidMount() {
     const url = `http://localhost:3000/api/${this.props.match.params.id}.json`;
     console.log(url);
+    console.log(url, 2);
     fetch(url)
       .then((res) => res.json())
       .then((json) => {
         console.log('Amanga', json);
+        const name = json.name;
+        const author = json.author;
+        this.setState({
+          name,
+          author,
+        });
       });
   }
   render() {
